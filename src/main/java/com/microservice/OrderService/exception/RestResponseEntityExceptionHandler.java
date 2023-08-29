44b176490bcddf893a635576e9b04031805915af
Fariz Sujatmiko <farizsujatmiko@gmail.com>
2023-08-29 15:50:17 +0700

    Tugas 1 Microservices

---
+++ src/main/java/com/microservice/OrderService/exception/RestResponseEntityExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.microservice.OrderService.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.microservice.OrderService.model.ErrorMessage;
+
+public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<ErrorMessage> customException(CustomException customException) {
+        return new ResponseEntity(new ErrorMessage().builder()
+                .message(customException.getMessage())
+                .error(customException.getError())
+                .build(),
+                HttpStatus.valueOf(customException.getStatus()));
+    }
+}
